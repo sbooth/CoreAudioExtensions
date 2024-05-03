98a79423f3d7fa92bfc640b30d4b37f0d1219a26
Stephen F. Booth <me@sbooth.org>
2024-05-03 15:04:27 -0500

    Spaces to tabs

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "CoreAudioExtensions",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "CoreAudioExtensions",
-            targets: ["CoreAudioExtensions"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CoreAudioExtensions"),
-        .testTarget(
-            name: "CoreAudioExtensionsTests",
-            dependencies: ["CoreAudioExtensions"]),
-    ]
+	name: "CoreAudioExtensions",
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.library(
+			name: "CoreAudioExtensions",
+			targets: ["CoreAudioExtensions"]),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
+			name: "CoreAudioExtensions"),
+		.testTarget(
+			name: "CoreAudioExtensionsTests",
+			dependencies: ["CoreAudioExtensions"]),
+	]
 )
