518406724a06c46cd1f12be8a7cfc9b069ad5f05
Stephen F. Booth <me@sbooth.org>
2024-07-06 08:50:43 -0500

    Update file header

--- Sources/CoreAudioExtensions/AudioValueRange+SFBExtensions.swift
+++ Sources/CoreAudioExtensions/AudioValueRange+SFBExtensions.swift
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2024 Stephen F. Booth <me@sbooth.org>
+// Copyright © 2024 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/CoreAudioExtensions
 // MIT license
 //
