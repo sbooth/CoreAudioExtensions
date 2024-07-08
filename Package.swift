19c0ee58e5fb6b8c783499083b9835ee49ca611b
Stephen F. Booth <me@sbooth.org>
2024-07-07 21:28:36 -0500

    Update formatting

--- Package.swift
+++ Package.swift
@@ -6,10 +6,11 @@ import PackageDescription
 let package = Package(
 	name: "CoreAudioExtensions",
 	products: [
-		// Products define the executables and libraries a package produces, making them visible to other packages.
 		.library(
 			name: "CoreAudioExtensions",
-			targets: ["CoreAudioExtensions"]),
+			targets: [
+				"CoreAudioExtensions",
+			]),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -18,6 +19,8 @@ let package = Package(
 			name: "CoreAudioExtensions"),
 		.testTarget(
 			name: "CoreAudioExtensionsTests",
-			dependencies: ["CoreAudioExtensions"]),
+			dependencies: [
+				"CoreAudioExtensions",
+			]),
 	]
 )
